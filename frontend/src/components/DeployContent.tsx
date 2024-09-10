f90f4fad7c56f87c26b1a61098520bd8f065711e
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-09-10 23:08:41 +0100

    feat: Add @wagmi/chains package and update dependencies

--- frontend/src/components/DeployContent.tsx
+++ frontend/src/components/DeployContent.tsx
@@ -1,13 +1,18 @@
+import MemoPasteIcon from "@/icons/PasteIcon";
 import CodeEditor from "./CodeMirrorEditor";
+import { Button } from "./ui/button";
 
 const DeployContent = () => {
   return (
-    <div>
+    <div className="relative">
       <CodeEditor
         defaultValue="//paste code here..."
         language="json"
         height="65vh"
       />
+      <Button className="absolute flex items-center space-x-2 top-3 right-4">
+        <MemoPasteIcon /> <p>Paste</p>
+      </Button>
     </div>
   );
 };
